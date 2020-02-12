c8f3e1bd99b471dd5e99972a89f2cb144bfc3fea
Tray Fleary <tflearydev@gmail.com>
2020-02-12 09:58:59 -0500

    setting up components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Homepage from './components/homepage/Homepage';
+import "bootstrap/dist/css/bootstrap.min.css";
 // import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+      <Router>
+
+      <Switch>
+            <Route exact path='/' component={Homepage} />
+          
+          </Switch>
+
+
+      </Router>
+    </>
   );
 }
 
