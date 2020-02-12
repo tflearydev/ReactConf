cbe5923145f26de10cca09f3053594e9dea5cc98
Tray Fleary <tflearydev@gmail.com>
2020-02-12 17:41:34 -0500

    installed shards react and font awesome

--- client/src/components/jumbotron/Jumbotron.jsx
+++ client/src/components/jumbotron/Jumbotron.jsx
@@ -8,16 +8,29 @@ import './Jumbotron.scss'
 function Jumbo() {
     return (
 
-        <Jumbotron className="jumbo">
+    
+      <section className="head">
+        
+       <Jumbotron className="jumbo">
+       <div className="overlay"></div>
+
+
+<div className="hello">
   <h1>Hello, world!</h1>
   <p>
     This is a simple hero unit, a simple jumbotron-style component for calling
     extra attention to featured content or information.
   </p>
+
   <p>
     <Button variant="primary">Learn more</Button>
   </p>
+  </div>
+
+
+
 </Jumbotron>
+</section>
     )
 }
 
